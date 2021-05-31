60ddb84c925337d15d8cc5543b5ba89d692177eb
hasnain <teachingtechzone@gmail.com>
2021-05-31 18:22:52 +0500

    adding first class

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 1. install (vscode) [https://code.visualstudio.com/]
 2. shift + !
 3. Download free (book) [https://cdn.wccftech.com/wp-content/uploads/2014/10/JavaScript.pdf]
+4. install an extension live server
--- app.js
+++ app.js
@@ -5,6 +5,20 @@
 
 // console.log(5+5);
 
-// alert(5+5)
+// alert(5+798*5)
 
 // window.alert(5-1)
+
+
+var hername = "My name is Maria";
+var city = "I live in Karachi"
+
+// console.log(hername)
+
+// concatinatino
+// console.log(hername + " " + city)
+
+var age = 19; // no need for inverted commas
+
+
+console.log(age)
