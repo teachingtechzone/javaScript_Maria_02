09cf57c10ea84441a9cd1ff8cfd20214e3b3a47e
hasnain <teachingtechzone@gmail.com>
2021-06-07 19:03:58 +0500

    class 3 added

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 3. Download free (book) [https://cdn.wccftech.com/wp-content/uploads/2014/10/JavaScript.pdf]
 4. install an extension live server
 5. Ctrl + /
+6. ctrl + click (file open)
+7. shift + alt + down/up arrow key (copy down or up)
+8. Alt + up/ down arrow key (move up or down)
---
+++ class3rd.js
@@ -0,0 +1,148 @@
+// prompts changing data type from string to number
+
+
+// var firstNumber = prompt("What is your first number");
+// var secondNumber = prompt("What is your second number");
+// // console.log(typeof(firstNumber)); // string
+
+// // changing datatype:
+// // var changeType = +firstNumber; // conversion first way
+// var changeType1 = parseInt(firstNumber); // conversion 2nd way
+// var changeType2 = parseInt(secondNumber); // conversion 2nd way
+
+// // console.log(typeof(changeType1)) // number
+
+// console.log(changeType1 +  changeType2 );
+
+
+
+// if, else if , else 
+
+
+
+// if(){
+    
+// }
+// else{
+        
+// }
+
+
+
+// var myname = "Maria";
+// if(myname == "maria"){ // if myname variable is equal to the string maria
+//     console.log("present");
+// }
+// else{
+//     console.log("Absent");
+// }
+
+
+
+// var number1 = 5;
+// var number1 = 7;
+
+// if(number1 == 5){
+//     console.log("number is equal to 5")
+// }
+// else {
+
+//     console.log("number is not equal to 5")
+
+// }
+
+
+// by maria
+
+// // prompt:
+// var firstnum = prompt("it is first number")
+// var secondnum = prompt("it is second number")
+// // change this string into number:
+// var newnum1 = +firstnum;
+// var newnum2 = +secondnum;
+
+// console.log(typeof(newnum1));
+// console.log(newnum1 + newnum2);
+
+// new
+//  var myname = "maria";
+//  if(myname == "maria"){
+//      console.log("pass")
+//  }
+//  else{
+//      console.log("fail")
+//  }
+
+
+// by teacher
+
+// if(){
+
+// }
+// else if(){
+
+// }
+// else {
+
+// }
+
+// var number1 = 5;
+// // var number1 = 7;
+// var number1 = 4;
+// if(number1 == 5){
+//     console.log("number is equal to 5")
+// }
+// else if(number1 > 5 ){
+//     console.log("number is greater than  5")
+    
+// }
+// else{
+    
+//     console.log("number is less than  5")
+// }
+// var number1 = 7;
+
+
+// var product = "C";
+// if(product == "L"){
+//     console.log("Your product is Laptop")
+// }
+// else if(product == "C"){
+//     console.log("Your product is Computer")
+    
+// }
+// else if(product == "M"){
+//     console.log("Your product is Mobile")
+    
+// }
+// else if(product == "Ch"){
+//     console.log("Your product is Charger")
+    
+// }
+// else{
+    
+//     console.log("We could not recongnize your product")
+// }
+
+var product = prompt("type your product");
+
+if(product == "L"){
+    console.log("Your product is Laptop")
+}
+else if(product == "C"){
+    console.log("Your product is Computer")
+    
+}
+else if(product == "M"){
+    console.log("Your product is Mobile")
+    
+}
+else if(product == "Ch"){
+    console.log("Your product is Charger")
+    
+}
+else{
+    
+    console.log("We could not recongnize your product")
+}
+
