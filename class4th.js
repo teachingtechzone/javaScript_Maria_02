811716f6c3d7315021940d1f9145eed00cf7b063
hasnain <teachingtechzone@gmail.com>
2021-06-09 18:52:20 +0500

    working on comparision operator and logical operators in if else

---
+++ class4th.js
@@ -0,0 +1,409 @@
+// Comparision operator
+
+// stric comparision
+
+// var num1 = "5";
+
+// var num2 = 5;
+
+// if(num1 == num2){
+//     console.log("they are equal");
+// }
+
+// else{
+//     console.log("they are not equal")
+// }
+
+
+// var num1 = "5";
+
+// var num2 = 5;
+
+// if(num1 === num2){ // will also check type
+//     console.log("they are equal");
+// }
+
+// else{
+//     console.log("they are not equal")
+// }
+
+
+
+
+
+
+
+
+
+
+// var num1 = "5";
+
+// var num2 = "5";
+
+// if(num1 === num2){ // will also check type
+//     console.log("they are equal");
+// }
+
+// else{
+//     console.log("they are not equal")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = "adf";
+
+// var num2 = "adf";
+
+// if(num1 === num2){ // will also check type
+//     console.log("they are equal");
+// }
+
+// else{
+//     console.log("they are not equal")
+// }
+
+
+
+
+
+
+
+
+
+
+// var num1 = 5;
+// var num2 = 4;
+
+// if(num1 != num2){
+//     console.log("true")
+// }
+// else{
+    
+//     console.log("false")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = 5;
+// var num2 = "5";
+
+// if(num1 !== num2){
+//     console.log("true")
+// }
+// else{
+    
+//     console.log("false")
+// }
+
+
+
+
+
+
+
+
+
+// var num1 = 55;
+
+// if(num1 != 55){ // false
+
+//     console.log("try again latter")
+
+// }
+
+// else{
+//     console.log("hurray")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// && , ||
+// var num1 = 5;
+// var num2 = 10 ;
+
+// if(num1 == 5 && num2 == 10){ // true
+//     console.log("the two conditions fulfilled")
+// }
+// else{
+//     console.log("failed")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = 51;
+// var num2 = 10 ;
+
+// if(num1 == 5 && num2 == 10){ // false
+//     console.log("the two conditions fulfilled")
+// }
+// else{
+//     console.log("failed")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = 5;
+// var num2 = 100 ;
+
+// if(num1 == 5 && num2 == 10){ // false
+//     console.log("the two conditions fulfilled")
+// }
+// else{
+//     console.log("failed")
+// }
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = 5;
+// var num2 = 100 ;
+
+// if(num1 == 5 || num2 == 10){ // true
+//     console.log("one of the two conditions fulfilled")
+// }
+// else{
+//     console.log("failed")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = 5;
+// var num2 = 1 ;
+
+// if(num1 >= 5 && num2 <= 10){ // true
+//     console.log("one of the two conditions fulfilled")
+// }
+// else{
+//     console.log("failed")
+// }
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = 5;
+// var num2 = 1 ;
+// var num3 = 6;
+
+// if(num1 == 5 || (num2 == 4 && num3 == 7)){ 
+//     console.log("one of the two conditions fulfilled")
+// }
+// else{
+//     console.log("failed")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = 5;
+// var num2 = 1 ;
+// var num3 = 6;
+
+// if(num1 == 5 && (num2 == 4 || num3 == 7)){ 
+//     console.log("one of the two conditions fulfilled")
+// }
+// else{
+//     console.log("failed")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// By Maria: (comparison operator)
+// by double equal to operator (datatype does not matter also not case sensitive):
+
+// var numberone= 8;
+// var numbertwo= 2;
+// if(numberone==8){
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+// }
+
+// by triple equal to operator(datatype matter):
+// var numberone= "8";
+// if(numberone===8){
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+// }
+
+// by not equal to operator:(datatype doesn't matter also case sensitive)
+// var paper= "English";
+// if(paper!="English"){
+//     console.log("it will be conducted")
+// }
+// else{
+//     console.log("it will be cancelled")
+// }
+// by double not equal to operator(datatype matter also case sensitive)
+// 
+// AND(&&) logical operator:
+// var product1= "computer";
+// var product2= "laptop";
+// if(product1=="computer" && product2=="laptop"){
+//     console.log("available")
+// }
+// else{
+//     console.log("not available")
+// }
+// // OR(||) operator:
+// var day1= "sunday";
+// var day2= "monday";
+// if( day1== "sunday" || day2=="saturday"){
+//     console.log("holiday")
+// }
+// else{
+//     console.log("not")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
