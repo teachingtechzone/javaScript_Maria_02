7b6a974bfb86bf1e8b7ec6cabb57861245697751
teachingtechzone <83701045+teachingtechzone@users.noreply.github.com>
2021-06-11 19:04:00 +0500

    5th class ended with nested if else and created simple calculator using prompt

---
+++ class5th.js
@@ -0,0 +1,132 @@
+// var age = 7;
+// var active = true;
+// var gender = "female";
+
+
+
+// if(age >= 18 && active == true && gender == "male"){
+//     console.log("yes you may go on picnic")
+// }
+// else{
+    
+//     console.log("No you are not allowed to go on picnic")
+// }
+
+
+
+
+
+// var age = 18;
+// var active = true;
+// var gender = "male";
+
+
+
+// if (age >= 18) { 
+//     if (active == true) {
+//         if (gender == "male") {
+//             console.log("yes you may go to picnic")
+//         } else {
+//             console.log("No you are not allowed because you are female")
+//         }
+//     } else {
+//         console.log("you are not allowed because you are not active student")
+//     }
+// } else {
+//     console.log("no you are not allowed because you are younger")
+// }
+
+
+
+// ######################################################//
+// ------------------- by maria -------------------------//
+// ######################################################//
+var age= 20;
+var active= "true";
+var gender= "male";
+
+if (age=>20) {
+    if (active="true") {
+        if (gender= "male") {
+            console.log("you can go to the picnic")
+        } else {
+            console.log("you cannot go because you are female")
+        }
+    } else {
+        console.log("you cannot go because you're not an active student")
+    }
+    
+} else {
+    console.log("you cannot go to the picnic because you are younger")
+}
+// ######################################################//
+// ------------------- by maria -------------------------//
+// ######################################################//
+//new ifelse:
+var names= "ayesha";
+var age= "18";
+var class1= "javascript";
+
+if (names=="ayesha") {
+    if (age==18) {
+        if (class1=="javascript") {
+            console.log("you can now enter in this class")
+        } else {
+            console.log("your class name doesn't match ")
+        }
+    } else {
+        console.log("your age doesn't match")
+    }
+} else {
+    console.log("your name doesn't match")
+}
+
+// ######################################################//
+// ------------------- by maria -------------------------//
+// ######################################################//
+//calculator:
+// var string1=prompt("what is your first number");
+// var operator=prompt("what is your operator");
+// var string2=prompt("what is your second number");
+
+// var num1= parseInt(string1);
+// var num2= parseInt(string2);
+
+// if (operator=="+") {
+//    console.log(num1+num2)
+// } 
+// else if(operator=="-"){
+//     console.log(num1-num2)
+// }
+// else if(operator=="*"){
+//     console.log(num1*num2)
+// }
+// else if(operator=="/"){
+//     console.log(num1/num2)
+// }
+// else {
+//     console.log("error")
+// }
+
+
+
+// ######################################################//
+// ------------------- by maria -------------------------//
+// ######################################################//
+var income = 50000;
+var famous = true;
+var situated = "karachi";
+
+if (income==50000) {
+    if (famous==true) {
+        if (situated=="karachi") {
+            console.log("you can get loan")
+        } else {
+            console.log("can't give you loan because you are out of required city")
+        }
+    } else {
+       console.log("can't give you loan because you are not working in famous company") 
+    }
+} else {
+    console.log("can't give you loan because of your low income")
+}
