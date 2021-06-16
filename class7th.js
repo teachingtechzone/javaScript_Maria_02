18ab1657261e95c8b8ef74290ddb38ffb69613bc
teachingtechzone <83701045+teachingtechzone@users.noreply.github.com>
2021-06-16 19:06:16 +0500

    added class 7th

---
+++ class7th.js
@@ -0,0 +1,106 @@
+// class 7th
+// loops
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+
+
+// console.log("My name is maria")
+// console.log("My name is maria")
+// console.log("My name is maria")
+// console.log("My name is maria")
+// console.log("My name is maria")
+// console.log("My name is maria")
+
+// for(var i = 0 ; i < 10 ; i++){
+
+//     console.log(i)
+
+// }
+
+// incrementing
+// for(var i = 1 ; i <= 10 ; i++){
+
+//     console.log(i)
+
+// }
+
+// // decrementing
+// for(var i = 10 ; i >= 0 ; i--){
+
+//     console.log(i)
+
+// }
+
+
+var myfruits = ["apple" , "mango" , "banana" , "orange"];
+
+// console.log(myfruits[0]);
+// console.log(myfruits[1]);
+// console.log(myfruits[2]);
+// console.log(myfruits[3]);
+
+
+// console.log(myfruits.length)
+// for (let index = 0; index < myfruits.length; index++) {
+    
+//     console.log(myfruits[index]);
+    
+// }
+
+
+
+
+
+//****************BY MARIA********************/
+//for Loops (used for stop repeative coding):
+
+// for(var element = 0 ; element < 10 ; element++){
+//     console.log(element)
+// }
+
+// for(var i = 9 ; i <= 10 ; i++){
+
+//         console.log(i)
+    
+//     }
+// for(var i = 10 ; i >= 0 ; i--){
+
+//     console.log(i)
+
+// }
+
+// for( var number = 8 ; number >= 5 ; number--){
+//     console.log(number)
+// }
+
+// var mysubjects = ["physics" , "chemistry" , "maths"];
+// console.log(mysubjects.length);
+// for (var a = 0; a < mysubjects.length; a++) {
+//     console.log(mysubjects[a])
+    
+// }
+var class2 = ["html" , "css" , "js"];
+// console.log(class2.length);
+// console.log(class2);
+// for (var i = 0; i < class2.length; i++) {
+    
+//     console.log("classe2")
+// }
+// console.log(myfruits.length)
+
+
+// for (let i = 0; i < class2.length; i++) {
+    
+//     // console.log(class2[i]);
+//     console.log(i)
+    
+// }
+
+// var table = 20;
+// // console.log(table.length);
+// for (var i = 0; i<=20 ; i++){
+//     console.log(i)
+// }
