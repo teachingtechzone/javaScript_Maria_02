c977aee4635e113e64503ba9458e9d4beef373f0
teachingtechzone <83701045+teachingtechzone@users.noreply.github.com>
2021-06-18 18:58:10 +0500

    added class 8th

---
+++ class8th.js
@@ -0,0 +1,87 @@
+// revision of array and for loop
+
+
+// var myfruits = ["apple", "orange", "banana"];
+
+// console.log(myfruits);
+// console.log(myfruits[0])
+// console.log(myfruits[1])
+// console.log(myfruits[2])
+
+// myfruits.forEach(elem => {
+//     console.log(elem)
+// });
+
+
+// for(var i = 0 ; i < myfruits.length ; i++){
+//     // console.log(i)
+//     console.log(myfruits[i])
+// }
+
+
+// printing data in index page
+// var li = document.getElementsByTagName("li");
+// // console.log(li)
+
+// // li[0].innerText = myfruits;
+// li[0].innerText = myfruits[0];
+
+
+
+//table:
+// var a = "maria";
+// console.log(a);
+//  var b = "2 x 1 = 2";
+//  console.log(b)
+
+//task(i have to print 10 times my name)
+
+// for (var i = 0 ; i  < 10 ; i++){
+//       console.log("element")
+// }
+// console.log("*******************")
+// for (var i = 10 ; i > 5 ; i--){
+//     console.log(i)
+// }
+// for (var i = 1 ; i <= 10 ; i++){
+//     console.log("maria")
+// }
+
+// for (var i = 1 ; i <= 10 ; i++){
+//     console.log("2 X 1 = 2")
+// }
+
+// for (var i = 1 ; i <= 10 ; i++){
+//     console.log("2 X " +i+ "=" + i*2 )
+//     // console.log(i)
+  
+// }
+// console.log("**********************")
+// for (var i = 1 ; i <= 10 ; i++){
+//     console.log("3 X " +i+ "=" + i*3)
+// }
+// console.log("**********************")
+// for ( var i = 10 ; i >= 1 ; i--){
+//     console.log( "2 x " + i + "=" + i*2)
+// }
+
+
+
+
+
+
+var table = prompt("What is your number");
+var num = parseInt(table);
+// var table = 10;
+for ( var i = 1 ; i<= 10 ; i++){
+    console.log( num + " x " + i + " = " + num*i)
+}
+
+
+
+
+
+
+
+
+
