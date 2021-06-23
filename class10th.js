d9616003f6c531f74773096b2a479d0db085d6e7
teachingtechzone <83701045+teachingtechzone@users.noreply.github.com>
2021-06-23 19:06:51 +0500

    class 10th added

---
+++ class10th.js
@@ -0,0 +1,205 @@
+// var cityInPakistan = ['karachi' , 'hyderabad' , 'lahore'];
+// var userCity = prompt("write your city name");
+
+
+
+// var check = false;
+// for(var i = 0 ; i < cityInPakistan.length ; i++){
+    
+//     if(userCity == cityInPakistan[i]){
+//         check = true;
+
+//     }
+
+// }
+
+
+
+// if(check == true){
+//     console.log('yes this city present in pakistan')
+// }
+// else{
+//     console.log("no this city is not present in pakitan")
+// }
+
+
+
+
+// // without break
+// for(var i = 0 ; i < cityInPakistan.length ; i++){
+    
+//     if(userCity == cityInPakistan[i]){
+//         console.log("yes this city is present in pakistan")
+//     }
+//     else{
+        
+//         console.log("No  this city is not present in pakistan")
+//     }
+
+// }
+
+
+
+
+// // with break
+// for(var i = 0 ; i < cityInPakistan.length ; i++){
+    
+//     if(userCity == cityInPakistan[i]){
+//         console.log("yes this city is present in pakistan");
+//         break;
+//     }
+//     else{
+//         console.log("No  this city is not present in pakistan")
+//     }
+
+// }
+
+
+
+
+
+
+// // with break
+// var check = false;
+// for(var i = 0 ; i < cityInPakistan.length ; i++){
+    
+//     if(userCity == cityInPakistan[i]){
+//         check = true;
+//         break;  
+//     }
+
+// }
+
+
+
+// if(check == true){
+//     console.log('yes this city present in pakistan')
+// }
+// else{
+//     console.log("no this city is not present in pakitan")
+// }
+
+
+
+// var text = "";
+// var i;
+// for (i = 0; i < 5; i++) {
+//   if (i === 3) {
+//     break;
+//   }
+// //   text += "The number is " + i + "\n";
+//   text = "The number is " + i + "\n";
+
+//   console.log(text);
+// }
+
+
+
+
+// nested for loops
+
+// // simple loop
+
+// for (let index = 0; index < 10; index++) {
+    
+//     console.log(index);
+    
+// }
+// // nested loop
+
+// for (let outerloop = 1; outerloop <= 5; outerloop++) {
+    
+//     for (let innerloop = 1; innerloop <= 3; innerloop++) {
+
+//         console.log("outer loop - " + outerloop + " inner loop - " + innerloop ); 
+        
+//     }
+    
+// task by sir using nested loop
+// (2,4) (3,4,5) = (2 x 3), (2 x 4), (2 x 5) , (4 x 3) , (4 x 4), (4 x 5)
+
+
+
+// for (let outerloop = 2; outerloop <= 4; outerloop++) {
+    
+//     for (let innerloop = 3; innerloop <= 5; innerloop++) {
+
+//         console.log(outerloop + " X " + innerloop ) 
+        
+//     }
+    
+// }
+
+// task by sir , printing pattern
+
+
+// var num = 5;
+
+// num++
+// num++ 
+
+// num = num + 1;
+// console.log(num)
+
+var a ; // declare a
+a = 45; // assign value
+
+var a = 45; // declare and assign
+
+// var chr = "";
+// for(var outerloop=1; outerloop <=6; outerloop++)
+// {
+//    for (var innerloop=1; innerloop < outerloop; innerloop++)
+//      {
+//             chr = chr + "*" ;        
+//       }
+
+//  console.log(chr);
+
+//  chr='';   
+ 
+ 
+// }
+
+
+
+// for(var outerloop=1; outerloop <=6; outerloop++)
+// {
+//    for (var innerloop=1; innerloop < outerloop; innerloop++)
+//      {
+//         console.log("*");
+//       }
+
+// }
+
+// var rows = 5;
+// for(var i = 1; i <= rows; i++){
+
+//     for(var j = 1; j <= i; j++){
+
+//     document.write(" * ");
+
+// }
+// document.write("<br/>");
+
+
+// } 
+
+
+
+
+// // concept for ascending descending
+// var a = 2;
+// var b = 3;
+// var c;
+
+// c = a;
+
+// a = b;
+
+// b = c ;
+
+
+// // a = 3, b = 2
+
+// console.log("a :" + a +" b :" + b);
